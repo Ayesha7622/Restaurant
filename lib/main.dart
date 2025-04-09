191ad64138da73076628735906f6eed6e794c6e5
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-09 23:29:01 +0500

    nnn

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant/container.dart';
 import 'package:restaurant/restaurant.dart';
 
 void main() {
@@ -11,7 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Restaurant());
+    return MaterialApp(
+        //home: Restaurant()
+        home: Containerclass());
   }
 }
 
