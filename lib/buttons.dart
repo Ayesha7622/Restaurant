4b2d1807fc73a61694df218012aa9a1f28e852ce
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-10 19:18:21 +0500

    gtfgg

---
+++ lib/buttons.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Buttonclass extends StatelessWidget {
+  const Buttonclass({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Center(
+        child: TextButton( 
+          child: Text('Add'),
+          onPressed: () {
+            int n1=6,
+            int n2=27,
+            print('n1=$n1');
+            print('n2=$n2');
+          },
+          ),
+          )
+    );
+  }
+}
