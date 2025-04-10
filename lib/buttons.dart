82b2ecbee2e38b355f7eb363c6946aa0f3483b19
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-10 22:54:53 +0500

    jfhjdsf

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -13,7 +13,7 @@ class Buttonclass extends StatelessWidget {
               child: Text(
                 'Addition',
                 style: TextStyle(
-                    fontSize: 15,
+                    fontSize: 30,
                     fontWeight: FontWeight.bold,
                     color: const Color.fromARGB(255, 185, 211, 255)),
               ),
