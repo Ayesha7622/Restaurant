63f5b9af81a2441297372c1a046e7ed9418a8364
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-10 20:13:07 +0500

    xccxcc

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -5,17 +5,16 @@ class Buttonclass extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
-        child: TextButton( 
-          child: Text('Add'),
-          onPressed: () {
-            int n1=6,
-            int n2=27,
-            print('n1=$n1');
-            print('n2=$n2');
-          },
-          ),
-          )
-    );
+        body: Center(
+      child: TextButton(
+        child: Text('Add'),
+        onPressed: () {
+          int n1 = 6;
+          int n2 = 27;
+          print('n1=$n1');
+          print('n2=$n2');
+        },
+      ),
+    ));
   }
 }
