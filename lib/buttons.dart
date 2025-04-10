7ec98a9e5c81a1104bdb036a56237c34bfc4d1d2
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-10 22:56:42 +0500

    mnbcxv

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -7,94 +7,97 @@ class Buttonclass extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 55, 45, 70),
       body: Center(
-        child: Column(
-          children: [
-            TextButton(
-              child: Text(
-                'Addition',
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 185, 211, 255)),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextButton(
+                child: Text(
+                  'Addition',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 185, 211, 255)),
+                ),
+                onPressed: () {
+                  int n1 = 6;
+                  int n2 = 27;
+                  int add = n1 + n2;
+                  print('n1=$n1');
+                  print('n2=$n2');
+                  print('Addition=$add');
+                },
               ),
-              onPressed: () {
-                int n1 = 6;
-                int n2 = 27;
-                int add = n1 + n2;
-                print('n1=$n1');
-                print('n2=$n2');
-                print('Addition=$add');
-              },
-            ),
-            TextButton(
-              child: Text(
-                'Subtraction',
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 238, 203, 176)),
+              TextButton(
+                child: Text(
+                  'Subtraction',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 238, 203, 176)),
+                ),
+                onPressed: () {
+                  int a = 54;
+                  int b = 12;
+                  int sub = a - b;
+                  print('a=$a');
+                  print('b=$b');
+                  print('Subtraction=$sub');
+                },
               ),
-              onPressed: () {
-                int a = 54;
-                int b = 12;
-                int sub = a - b;
-                print('a=$a');
-                print('b=$b');
-                print('Subtraction=$sub');
-              },
-            ),
-            TextButton(
-              child: Text(
-                'Multiplication',
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 232, 194, 227)),
+              TextButton(
+                child: Text(
+                  'Multiplication',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 232, 194, 227)),
+                ),
+                onPressed: () {
+                  int z = 16;
+                  int x = 27;
+                  int multi = z * x;
+                  print('z=$z');
+                  print('x=$x');
+                  print('Multiplication=$multi');
+                },
               ),
-              onPressed: () {
-                int z = 16;
-                int x = 27;
-                int multi = z * x;
-                print('z=$z');
-                print('x=$x');
-                print('Multiplication=$multi');
-              },
-            ),
-            TextButton(
-              child: Text(
-                'Division',
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 232, 240, 172)),
+              TextButton(
+                child: Text(
+                  'Division',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 232, 240, 172)),
+                ),
+                onPressed: () {
+                  double m = 69;
+                  double n = 27;
+                  double div = m / n;
+                  print('m=$m');
+                  print('n=$n');
+                  print('Division=$div');
+                },
               ),
-              onPressed: () {
-                double m = 69;
-                double n = 27;
-                double div = m / n;
-                print('m=$m');
-                print('n=$n');
-                print('Division=$div');
-              },
-            ),
-            TextButton(
-              child: Text(
-                'Modulas',
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: const Color.fromARGB(255, 174, 193, 226)),
+              TextButton(
+                child: Text(
+                  'Modulas',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 174, 193, 226)),
+                ),
+                onPressed: () {
+                  double n1 = 6;
+                  double n2 = 27;
+                  double mod = n1 % n2;
+                  print('n1=$n1');
+                  print('n2=$n2');
+                  print('Modulas=$mod');
+                },
               ),
-              onPressed: () {
-                double n1 = 6;
-                double n2 = 27;
-                double mod = n1 % n2;
-                print('n1=$n1');
-                print('n2=$n2');
-                print('Modulas=$mod');
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
