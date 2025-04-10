8ea3b6b18fc47fd56805b2470e8c1776f39099ff
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-10 14:15:48 +0500

    xxcvvbn

--- lib/container.dart
+++ lib/container.dart
@@ -457,7 +457,7 @@ class Containerclass extends StatelessWidget {
                     height: 450,
                     width: 550,
                     decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 180, 237, 244),
+                      color: const Color.fromARGB(255, 158, 232, 209),
                       borderRadius: BorderRadius.circular(15),
                     ),
                     child: Column(
