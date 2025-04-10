1c2aa0c658ffbea165af1b87b633a8f23ccc55b4
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-10 22:55:52 +0500

    jdhf

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -64,7 +64,7 @@ class Buttonclass extends StatelessWidget {
               child: Text(
                 'Division',
                 style: TextStyle(
-                    fontSize: 15,
+                    fontSize: 30,
                     fontWeight: FontWeight.bold,
                     color: const Color.fromARGB(255, 232, 240, 172)),
               ),
