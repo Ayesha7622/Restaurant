087404f16136262f8385e5ff9e94ebc73010f2c4
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-11 22:38:27 +0500

    jhgh

--- lib/iconsbutton.dart
+++ lib/iconsbutton.dart
@@ -111,7 +111,7 @@ class Iconclass extends StatelessWidget {
                           child: Text(
                             '*',
                             style: TextStyle(
-                                fontSize: 30,
+                                fontSize: 40,
                                 fontWeight: FontWeight.bold,
                                 color:
                                     const Color.fromARGB(255, 232, 240, 172)),
