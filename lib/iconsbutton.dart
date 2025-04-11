000801170169d195fb331a531ef28c28447891ad
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-11 22:45:25 +0500

    shdys

--- lib/iconsbutton.dart
+++ lib/iconsbutton.dart
@@ -36,8 +36,8 @@ class Iconclass extends StatelessWidget {
                         onPressed: () {},
                         icon: Icon(
                           Icons.add,
-                          size: 30,
-                          color: const Color.fromARGB(255, 117, 165, 135),
+                          size: 40,
+                          color: const Color.fromARGB(255, 216, 127, 144),
                         ),
                       ),
                     ],
