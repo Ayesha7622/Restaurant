c843b67ae6a83b38f4916895e14bcee1fbc6ab80
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-12 22:38:41 +0500

    jdbdm

--- lib/result sheet.dart
+++ lib/result sheet.dart
@@ -6,17 +6,45 @@ class Iconclass extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color.fromARGB(255, 52, 94, 78),
-        body: Column(
-          children: [
-            Row(children: [
-              Text('Subject Marks:'),
-              Row(
-                children: [
-                  Text('English = 95/100'),
-                ],
-              )
-            ])
-          ],
-        ));
+        body: Column(children: [
+          Row(children: [
+            Text('Subject Marks:'),
+          ]),
+          Row(
+            children: [
+              Text('English = 75/100'),
+            ],
+          ),
+          Row(
+            children: [
+              Text('Chemistry = 95/100'),
+            ],
+          ),
+          Row(
+            children: [
+              Text('Islamiyat = 44/50'),
+            ],
+          ),
+          Row(
+            children: [
+              Text('Physics = 90/100'),
+            ],
+          ),
+          Row(
+            children: [
+              Text('Urdu = 88/100'),
+            ],
+          ),
+          Row(
+            children: [
+              Text('Biology = 89/100'),
+            ],
+          ),
+          Row(
+            children: [
+              Text('English = 98/100'),
+            ],
+          ),
+        ]));
   }
 }
