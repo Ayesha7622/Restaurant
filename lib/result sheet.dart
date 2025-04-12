ad41c4d9a4ae8c16dcac9aacff3dfadd4bfefbf5
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-12 22:31:53 +0500

    ydfsdh

---
+++ lib/result sheet.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Iconclass extends StatelessWidget {
+  const Iconclass({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 52, 94, 78),
+        body: Column(
+          children: [
+            Row(children: [
+              Text('Subject Marks:'),
+            ])
+          ],
+        ));
+  }
+}
