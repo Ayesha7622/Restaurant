3a81d51da67fcdad98051cb48ac0fccd0263735d
Ayesha7622 <saddiqa7676@gmail.com>
2025-04-13 21:00:07 +0500

    hwsnb

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Iconclass()
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        //home: Iconclass()
         //home: Buttonclass()
-        //home: Restaurant()
+        home: Restaurant()
         //home: Containerclass()
         );
   }
